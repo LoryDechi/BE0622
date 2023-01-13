4b79c395b6d4bd4cd55ac4f51a17f084b72e40c6
Lorenzo De Carolis <lorenzodc@hotmail.it>
2023-01-13 17:00:43 +0100

    Update Main.java

--- week-1/progettoSettimana1/src/elementiMultimediali/Main.java
+++ week-1/progettoSettimana1/src/elementiMultimediali/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		ElementoMultimediale[] elementi = new ElementoMultimediale[1];
+		ElementoMultimediale[] elementi = new ElementoMultimediale[5];
 		int stop = elementi.length;
 		int indice = 0;
 
